ed410e6f1ac58498ef5724ff234ec4fe8d6802d5
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-17 15:00:28 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0", n, last_dig);
 	}
-	else if (last_dig < 6 && != 0)
+	else if (last_dig < 6 && last_dig != 0)
 	{
 		printf("Last digit of %d is %d and and is less than 6 and not 0", n, last_dig);
 	}
