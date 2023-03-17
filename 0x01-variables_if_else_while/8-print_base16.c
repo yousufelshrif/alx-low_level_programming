b4d8d23a5efd1f482bf5615059cea02c3eee85fb
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-17 16:47:47 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - "Write a program that prints all the numbers of
@@ -7,11 +7,16 @@
  */
 int main(void)
 {
+	char i;
 	int num;
 
-	for (num = 48; num <= 64; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
+	}
+	for (i = 'a';i <= 'f'; i++)
+	{
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
