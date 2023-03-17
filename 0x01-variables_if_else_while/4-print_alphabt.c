68da7c0c63e64599cc95c89d11a223e79415e689
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-17 15:57:19 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - "Write a program that prints the alphabet in lowercase, followed by a new line.
+ * main - "Write a program that prints the alphabet in lowercase,
+ * followed by a new line.
  * Return: Always 0
  */
 int main(void)
 {
 	char i;
+
 	for (i = 97; i <= 122; i++)
 	{
 		if (i != 113 || i != 101)
