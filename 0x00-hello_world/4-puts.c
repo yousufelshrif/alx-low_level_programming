b785573e6f029d279d81164e5deb5a56a7ac1ea8
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-16 17:35:34 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 /**
  * main - prints "Programming is like building a multilingual puzzle"
+ * return 0.
 */
- int main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
