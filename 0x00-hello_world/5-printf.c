6e142eeacafe1f808209d04b700481d6022a9f32
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-16 17:46:38 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+
+/**
+ * main - "prints ith proper grammar, but the outcome is a piece of art,
+ *  followed by a new line.
+ *  Return: Always 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
