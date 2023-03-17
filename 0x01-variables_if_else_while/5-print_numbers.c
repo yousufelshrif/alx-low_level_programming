ffd164b165aca600be181b5f4cd8a139e5b10298
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-17 16:06:19 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - "Write a program that prints all single digit numbers of
+ * base 10 starting from 0, followed by a new line.
+ * Return: Always 0
+ */
+int main(void)
+{
+        int i;
+
+        for (i = 0; i < 10; i++)
+        {
+		putchar(i);
+        }
+                putchar('\n');
+        return (0);
