32041edd57ebd9c428d3622ceff8be4af5e94587
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-16 18:11:39 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,16 @@
 
 
 /**
- * main - "prints the size of various types on the computer it is compiled and run on.
+ * main - "prints the size of various types on the computer
+ * it is compiled and run on.
  * Return: Always 0
  */
 int main(void)
 {
-	printf("Size of a char: %d  byte(s)",sizeof(char));
-	printf("Size of an int: %d  byte(s)",sizeof(int));
-	printf("Size of a long int: %d  byte(s)",sizeof(long int));
-	printf("Size of a long long int: %d  byte(s)",sizeof(long long int));
-	printf("Size of a float: %d  byte(s)",sizeof(float));
+	printf("Size of a char: %d  byte(s)\n", sizeof(char));
+	printf("Size of an int: %d  byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d  byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d  byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d  byte(s)\n", sizeof(float));
 	return (0);
 }
