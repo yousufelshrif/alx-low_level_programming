a666ac117f5b71fce7691a1184913780d68a3b6a
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-17 15:31:30 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - "program that prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line.
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i <= 90; i++)
+        {
+                putchar(i);
+        }
+	putchar('\n');
+
+	return (0);
+}
