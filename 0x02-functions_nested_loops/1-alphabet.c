edfd3b305f224fd978bee7498c415693c053a468
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-21 16:09:35 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,12 @@
  */
 
 void print_alphabet(void)
-{       
-        char i;
+{
+	char i;
 
-        for (i = 'a'; i <= 'z'; i++)
-        {
-                _putchar(i);
-        }
-        _putchar('\n');
+	for (i = 'a'; i <= 'z'; i++)
+	{	
+		_putchar(i);
+	}
+		_putchar('\n');
 }       
