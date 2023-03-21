d7be8662e36af535fc7ad2af3b243b3905eb6a39
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-21 16:54:53 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _islower (int c) - "Write a function that checks
+ * for lowercase character"
+ * Return: 1 if c is lowercase and 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c<= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	{
+}
