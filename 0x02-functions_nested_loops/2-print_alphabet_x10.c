aa05380b5289a1910ed96be620a5ddc9d6588e09
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-21 16:42:45 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,13 @@ void print_alphabet_x10(void)
 {
 	int i,j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
+		_putchar('\n')
 	}
-	_putchar('\n');
+	
 }
