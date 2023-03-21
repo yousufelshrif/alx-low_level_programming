cf4ae8dc77a324cc91615bd47e4cadc977bc93b3
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-21 16:10:57 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - "Write a function that prints the alphabet,
+ * print_alphabet - "Write a function that prints the alphabet,
  *  in lowercase, followed by a new line
  *  Return: Always 0
  */
