7e551023fab51b153ef4961d5975cc12e4620df8
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-21 16:30:50 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - "Write a function that
+ * prints10 times the alphabet"
+ * Returs: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i,j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
