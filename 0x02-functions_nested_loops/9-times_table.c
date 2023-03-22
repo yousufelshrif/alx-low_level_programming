570c39b28eaa39fd5466a85b24ba054aff115124
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-22 10:29:04 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - "Write a function that prints the 9
+ * times table, starting with 0"
+ * ReturnAl: Always void
+ */
+void times_table(void)
+{
+	int i, j, a, b, c;
+
+	for (i = 0; i <= 9; i++)
+	{ 
+	for (j = 0; j <=9; j++)
+	{
+		a = i * j;
+		if (a > 9)
+		{
+		b = a % 10;
+		c = (a - b) / 10;
+		}
+		_putchar(',');
+		_putchar(' ');
+		_putchar(c + '0');
+		_putchar(b + '0');
+
+	}
+	else
+	{
+		if (j != 0)
+		{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		}	
+		_putchar(a, '0');
+	}
+		_putchar('\n');
+	}
+}
