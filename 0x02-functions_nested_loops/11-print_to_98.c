0ebe377412cf80cb03183845e9d5d4d3e186634b
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-22 13:04:59 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - "Write a function that prints all natural
+ * numbers from n to 98, followed by a new line."
+ * Return: Always void
+ */
+void print_to_98(int n)
+{
+	for (; n <= 98; n++)
+	{
+	printf("%d ,", n);
+	}
+}
