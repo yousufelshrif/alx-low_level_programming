a397e740d910ab5ff6cf1921900310d37d40f59e
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-21 17:51:06 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - "Write a function that computes the absolute value of an integer."
+ * @n: the number to be cheaked
+ * Return: Always abs of the number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+		return (n);
+}
