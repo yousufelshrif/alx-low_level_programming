b3f385a669a1d6ee3014d380358ffd216be3562b
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-21 17:37:31 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,21 +2,21 @@
 
 /**
  * print_sign - "Write a function that prints the sign of a number."
- * @c: the char to be checked
+ * @n: the char to be checked
  * Return: 1 if + -1 if - and 0 if 0
  */
 int print_sign(int n)
 {
-        if (n > 0)
-        {
+	if (n > 0)
+	{
 		_putchar('+');
-                return (+1);
-        }
-        else if (n < 0)
-        {
+		return (+1);
+	}
+	else if (n < 0)
+	{
 		_putchar('-');
-                return (-1);
-        }
+		return (-1);
+	}
 	else
 	{
 		_putchar('0');
