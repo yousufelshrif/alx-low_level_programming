23bec56317409fa16c838134495556ac09c6820f
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-23 14:25:29 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+
+/**
+ * _isdigit - cheak if the passed number is uppercase ascii
+ * @c: the number passed to be tested
+ * Return: 0 if lowercase and 1 if uppercase
+ */
+int _isdigit(int c)
+{
+	if (_isdigit(int c))
+	{
+		return (1);
+	}
+	else
+	return (0);
+}
