523d66e5e04792d4657cfdf70c7441154a39cb7a
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-23 14:11:07 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include <ctype.h>
+
+/**
+ * _isupper - cheak if the passed number is uppercase ascii
+ * @c: the number passed to be tested
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	return (isupper(c));
+}
