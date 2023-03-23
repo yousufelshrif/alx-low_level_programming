dce90d8693687b37f5b8f41ac74d7d0af88d3310
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-23 16:44:44 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,22 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, a, b, c;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
+			if (j > 9)
+			{
+				a = j % 10;
+				b = j - a;
+				_putchar(b + '0');
+				_putchar(a + '0');
+			}
+			else
 			_putchar(j + '0');
 		}
 		_putchar('\n');
 	}
-		_putchar('\n');
 }
