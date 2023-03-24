85a53dcb54ad59ab16f60ae1d284f7b4b0de1b42
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-23 17:31:53 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints / n times
+ * @n: the number of printed _
+ */
+
+void print_diagonal(int n)
+{
+	int i,j;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < n; i++)
+	{
+	for (j = 0; j < x; j++)
+	{
+	_putchar(32);
+	}
+	_putchar(92);
+	_putchar('\n');
+	}
+	}
+}
