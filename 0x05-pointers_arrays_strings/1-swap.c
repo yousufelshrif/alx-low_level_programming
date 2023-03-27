28e039c8bba3762b2b39cd0f665e8ae91c38c422
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-27 14:45:23 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - function that swaps the values of two integers
+ * @a: number to be swaped
+ * @b: number to be swaped
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *tmp = a;
+
+	a = b;
+	b = tmp;
+}
