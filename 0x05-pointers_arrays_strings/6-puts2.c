8060f331b10787c75db68bdf62a4713555b6b0b6
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-27 16:43:28 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string, starting with the first character
+ * @str: string be printed
+ */
+
+void puts2(char *str)
+{
+	int len = 0, index;
+
+	while (str[len++])
+	{
+		len++;
+	}
+	for (index = 0; index <= len -1 && str[index++] ; index += 2)
+	{
+		_puts(str[index]);
+	}
+}
