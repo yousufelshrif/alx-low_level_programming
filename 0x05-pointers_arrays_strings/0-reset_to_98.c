7c3c89b936171fdd3c0799e4b6aba3f6fc215760
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-27 14:36:15 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 /**
  * reset_to_98 - function that takes a pointer to an int as
  * parameter and updates the value it points to to 98
+ * @n: pointer to the integar parametar to be changed
  */
 
 void reset_to_98(int *n)
