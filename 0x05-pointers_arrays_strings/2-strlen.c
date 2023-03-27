6a2d652f55d3c0f835ce7500d03d32b88cd73099
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-27 15:10:17 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: array of char to be counted
+ * Return: the length of string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
