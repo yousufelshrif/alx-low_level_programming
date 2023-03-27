13698cecf175c5604c3784888fcff524ffcdcfb4
yousuf ahmed <git@github.com:yousufelshrif/alx-zero_day.git>
2023-03-27 15:22:58 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * print_rev -  prints a string, in reverse, followed by a new line.
+ * @s: pointer to the string to be printed in reverse
+ */
+
+void print_rev(char *s)
+{
+	strrev(s);
+}
